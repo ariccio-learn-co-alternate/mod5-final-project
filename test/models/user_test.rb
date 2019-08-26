e2cb6b5be0e62a5a3f6d8334d1e53649b4596e92
Alexander Riccio <test35965+learn@gmail.com>
2019-08-26 17:59:28 -0400

    Copied some stuff over from mod4
    It's all the same, and I don't want to do it wrong!

---
+++ test/models/user_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'has at least a user' do
+    assert !(User.all.empty?)
+    assert_not_nil User.first.username
+  end
+end
