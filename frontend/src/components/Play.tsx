55586a937ffd8f58ebad0ecb316fa06767e2d1b3
Alexander Riccio <test35965+learn@gmail.com>
2019-09-04 11:31:19 -0400

    render to offscreen canvas

--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -18,65 +18,132 @@ const defaultCanvasState: CanvasState = {
     // ctx: null
 }
 
+function canvasIDString(index: number): string {
+    return `${CANVAS_ID}-${index}`;
+}
+function initCanvas(canvas: HTMLCanvasElement, width: number, height: number, hidden: boolean): void {
+    canvas.width = width;
+    canvas.height = height;
+    canvas.hidden = hidden;
+}
+
+
 const CANVAS_CONTAINER: string = "game-canvas-container";
 const CANVAS_ID: string = "game-canvas-element-id";
 const container: HTMLDivElement = document.getElementById(CANVAS_CONTAINER) as HTMLDivElement;
-const gameCanvas: HTMLCanvasElement = document.createElement('canvas');
-gameCanvas.id = CANVAS_ID;
+const gameCanvas_0: HTMLCanvasElement = document.createElement('canvas');
+// const gameCanvas_1: HTMLCanvasElement = document.createElement('canvas');
+let offscreenCanvas: OffscreenCanvas|null = null
 
 
-// WHY U NO WORK
-// gameCanvas.style.border = '100px';
-// gameCanvas.style.borderColor = 'black';
 
-gameCanvas.width = 400;
-gameCanvas.height = 400;
-gameCanvas.hidden = false;
-container.appendChild(gameCanvas);
+gameCanvas_0.id = canvasIDString(0);
+// gameCanvas_1.id = canvasIDString(1);
 
-function getCanvasCtx(): CanvasRenderingContext2D {
-    const canvas: HTMLCanvasElement = document.getElementById(CANVAS_ID) as HTMLCanvasElement;
+initCanvas(gameCanvas_0, 400, 400, false);
+// initCanvas(gameCanvas_1, 400, 400, true);
+container.appendChild(gameCanvas_0);
+
+function getCanvasCtx(): any {
+    const canvas: HTMLCanvasElement = document.getElementById(canvasIDString(0)) as HTMLCanvasElement;
     if (canvas === null) {
-        console.log("null canvas")
+        console.error("null canvas")
         throw new Error();
     }
-    const ctx = canvas.getContext("2d")
+    const ctx: any = canvas.getContext("bitmaprenderer");
     if (ctx === null) {
-        console.log("null context");
+        console.error("null context");
         throw new Error();
     }
     return ctx
 }
 
+function getOffscreenContext(width: number, height: number): OffscreenCanvasRenderingContext2D {
+    offscreenCanvas = new OffscreenCanvas(width, height);
+    const context = offscreenCanvas.getContext('2d') as OffscreenCanvasRenderingContext2D;
+    if (context === null) {
+        console.error("null context")
+        throw new Error();
+    }
+    return context;
+}
+
+interface CanvasProps {
+
+}
+
 // https://blog.cloudboost.io/using-html5-canvas-with-react-ff7d93f5dc76
-class Canvas extends React.Component<{}, CanvasState> {
-    state = defaultCanvasState;
-    ctx = getCanvasCtx();
-    x = 0;
-    y = 0;
+class Canvas extends React.Component<CanvasProps, CanvasState> {
+    state: any;
+    ctx: any;
+    offscreenContext: OffscreenCanvasRenderingContext2D;
+    lastBitmapRenderedBool: boolean;
+    bitmap_0: ImageBitmap | null;
+    bitmap_1: ImageBitmap | null;
+    x: number;
+    y: number;
     timer: any;
 
+    constructor(props: CanvasProps) {
+        super(props);
+        this.state = defaultCanvasState;
+        this.ctx = getCanvasCtx();
+        this.offscreenContext = getOffscreenContext(400, 400);
+        this.lastBitmapRenderedBool = false;
+        this.bitmap_0 = null;
+        this.bitmap_1 = null;
+    
+        this.x = 0;
+        this.y = 0;
+    
+    }
+
+    renderToContextFromBitmap() {
+        if (offscreenCanvas === null) {
+            console.warn("null canvas");
+            return;
+        }
+        if (this.lastBitmapRenderedBool) {
+            this.bitmap_0 = offscreenCanvas.transferToImageBitmap();
+            this.ctx.transferFromImageBitmap(this.bitmap_0)    
+        }
+        else {
+            this.bitmap_1 = offscreenCanvas.transferToImageBitmap();
+            this.ctx.transferFromImageBitmap(this.bitmap_1)
+        }
+        this.lastBitmapRenderedBool = !(this.lastBitmapRenderedBool);
+
+    }
+
     step() {
-        this.ctx.clearRect(0,0, 400, 400);
-        this.ctx.fillText("Not implemented yet.", this.x, this.y)
-        this.ctx.fillText(".tey detnemelpmi toN", (-this.x)+gameCanvas.width, (-this.y)+gameCanvas.height)
+        if (offscreenCanvas === null) {
+            console.warn("null canvas");
+            return;
+        }
+        // this.offscreenContext.clearRect(0,0, 400, 400);
+        this.offscreenContext.fillText("Not implemented yet.", this.x, this.y)
+        this.offscreenContext.fillText(".tey detnemelpmi toN", (-this.x)+gameCanvas_0.width, (-this.y)+gameCanvas_0.height)
         this.x = ((this.x + 1) % 400);
         this.y = ((this.y + 2) % 400);
-        
+        this.renderToContextFromBitmap();
     }
 
     componentDidMount() {
-        if (this.ctx === null) {
+        if ((this.ctx === null) || (this.offscreenContext === null) || (offscreenCanvas === null)) {
             return;
         }
-        this.ctx.fillText("Not implemented yet.", 200, 200)
+        this.offscreenContext.fillText("Not implemented yet.", 200, 200)
         this.timer = setInterval(this.step.bind(this), 10);
-        gameCanvas.hidden = false;
+        gameCanvas_0.hidden = false;
+        this.renderToContextFromBitmap();
+
     }
 
     componentWillUnmount() {
         clearInterval(this.timer)
-        gameCanvas.hidden = true;
+        gameCanvas_0.hidden = true;
+        // this.bitmap_0.close();
+        // this.bitmap_1.close();
     }
 
     render() {
@@ -88,9 +155,7 @@ class Canvas extends React.Component<{}, CanvasState> {
 class _Play extends React.Component<PlayProps, PlayState> {
     render() {
         return (
-        <>
             <Canvas/>
-        </>
         );
     }
 }
