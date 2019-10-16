9d97c6a55815f83c1a3b50a6f9c14fe4be2e0026
Alexander Riccio <test35965+learn@gmail.com>
2019-10-16 18:30:49 -0400

    Revert "forgot a return duh"
    This reverts commit 799493147f1c5efa17abc064ef4296ff71596bff.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,7 +70,6 @@ class ApplicationController < ActionController::API
       logger.info "rendering public/index.html from #{request.url}..."
       respond_to do |format|
         format.html { render body: Rails.root.join('public/index.html').read }
-        return
       end
     end
     if !(logged_in?)
